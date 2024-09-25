c1b48b28a9174c23edcc9722d16f2c0828882cc3
Breno Mororó <86978303+Btmafonso@users.noreply.github.com>
2024-09-24 21:22:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+
+// Adiciona scroll suave ao clicar nos links de navegação
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
